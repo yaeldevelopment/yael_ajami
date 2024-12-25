2046e5ccb77d7aa8ddeb596c4291af0e2b3cddd7
yaelajami2 <y0556722091@gmail.com>
2024-12-25 11:13:29 +0200

    stopThinking

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -20,9 +20,12 @@ export class ManageComponent {
 constructor(private serv:QuionService){
   
  this.serv.currentValue$.subscribe((value) => {
-  this.isThinking=true;
- this.stopThinking();
-      this.currentValue = value;
+  if(value.length>0){
+    this.isThinking=true; 
+    this.stopThinking();
+    this.currentValue = value;
+  }
+      
 
     });
 
